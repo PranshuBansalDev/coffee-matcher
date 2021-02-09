c4c54b85939ac89999b450841857de0af1bf3ba1
Pranshu Bansal <prbans@microsoft.com>
2021-02-08 17:28:18 -0800

    Account for first/last name, make output easier to use by mail merge

--- produce-matches.py
+++ produce-matches.py
@@ -1,65 +1,96 @@
+import os
 import itertools
-import csv
 
 import pandas as pd
 
-empty_slot = "-"
+# convenience constants
+matches = "matches"
+people = "people"
+
+# Inputs
+dataset_id = 7
+
+# Filenames
+ifile_name = f'./inputs/test-dataset-{dataset_id}.csv'
+ofile_dir = f'./outputs/input-{dataset_id}'
+
+if not os.path.exists(ofile_dir):
+    os.mkdir(ofile_dir)
+
+
+# Header info
+firstName_col = "firstName"
+lastName_col = "lastName"
+email_col = "email"
 
 def i_to_str(i: int, prefix: str="Week"):
     return f'{prefix}_{i+1:03d}'
 
-def get_email_arr(file_path: str, header: str) -> set:
-    rv = set()
-    with open(file_path, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            rv.add(row[header])
+def find_first_avail_week(weeks: dict, person_a: str, person_b: str):
+    for val in weeks.values():
+        if (person_a not in val[people]) and (person_b not in val[people]):
+            return val
+    
+    raise "AAHHH"
 
-    return rv       
 
-def generate_table(emails: list, combos: list):
-    weeks = [i_to_str(i) for i in range(0, len(combos))]
-    d = {week: [empty_slot for email in emails] for week in weeks}
-    d["Emails"] = emails
-    df = pd.DataFrame(data=d)
-    df.set_index("Emails", inplace=True)
+df = pd.read_csv(ifile_name)
 
-    df = df.reindex(sorted(df.columns), axis=1)
-    df = df.sort_index()
+# Extrapolated info
+seenBefore_col = 'seenBefore'
+mergedId_col = "mergedId"
 
-    return df
+df[mergedId_col] = df.apply(lambda row: (f"{row[email_col]}|{row[firstName_col]}|{row[lastName_col]}"), axis=1)
+df[seenBefore_col] = df.apply(lambda x: [], axis=1)
 
-def fill_table(table: pd.DataFrame, combos: list):
-    for i in range(len(combos)):
-        combo = combos[i]
+# Figure out all possible pairings
+combos = set(itertools.combinations(df[mergedId_col].to_list(), 2))
+
+# max number of weeks for this to happen over (e.g. 1 match per week, but we can do better than that)
+# consider these placeholders
+weeks = {i_to_str(i):{matches: [], people: []} for i in range(0, len(combos))}
+
+## core matching algorithm
+used_combos = set()
+available_combos = combos.difference(used_combos)
+while len(available_combos) > 0:
+    for combo in available_combos:
         person_a = combo[0]
         person_b = combo[1]
+
+        person_a_seen_before = df[df[mergedId_col] == person_a].iloc[0][seenBefore_col]
+        person_b_seen_before = df[df[mergedId_col] == person_b].iloc[0][seenBefore_col]
+
+        have_met = (person_b in person_a_seen_before) or (person_a in person_b_seen_before)
+
+        if not have_met:
+            week = find_first_avail_week(weeks, person_a, person_b)
+            week[people].append(person_a)
+            week[people].append(person_b)
+            week[matches].append(combo)
+            used_combos.add(combo)
         
-        person_a_free_slots = table.loc[person_a, :] == empty_slot
-        person_b_free_slots = table.loc[person_b, :] == empty_slot
+    available_combos = combos.difference(used_combos)
 
-        free_slots = person_a_free_slots & person_b_free_slots
-        first_free_slot = free_slots[free_slots == True].index[0]
+## Now that we have the matches, clean it up and make it look pretty
+# Then output to csv
+for week in weeks:
+    ofile_name = ofile_dir + f"/{week}.csv"
+    df = pd.DataFrame(weeks[week]['matches'])
 
-        table.loc[person_a, first_free_slot] = person_b
-        table.loc[person_b, first_free_slot] = person_a
+    if not df.empty:
+        df.columns = ['person_a', 'person_b']
+        df['split_a'] = df.apply(lambda row: row['person_a'].split("|"), axis=1)
+        df['split_b'] = df.apply(lambda row: row['person_b'].split("|"), axis=1)
 
-    return table
+        df['person_a_email'] = df.apply(lambda row: row['split_a'][0], axis=1)
+        df['person_a_firstName'] = df.apply(lambda row: row['split_a'][1], axis=1)
+        df['person_a_lastName'] = df.apply(lambda row: row['split_a'][2], axis=1)
 
-def drop_empty_cells(table: pd.DataFrame):
-    empty_cols = [col for col in table.columns if all(table[col] == empty_slot)]
-    table.drop(empty_cols, axis=1, inplace=True)
-    return table
+        df['person_b_email'] = df.apply(lambda row: row['split_b'][0], axis=1)
+        df['person_b_firstName'] = df.apply(lambda row: row['split_b'][1], axis=1)
+        df['person_b_lastName'] = df.apply(lambda row: row['split_b'][2], axis=1)
 
-dataset_id = 7
-ifile_name = f'./inputs/test-dataset-{dataset_id}.csv'
-ofile_name = f'./outputs/output-dataset-{dataset_id}.csv'
-
-emails = list(get_email_arr(ifile_name, 'Email'))
-combos = list(itertools.combinations(emails, 2))
-
-table = generate_table(emails, combos)
-table = fill_table(table, combos)
-table = drop_empty_cells(table)
-
-table.to_csv(ofile_name)
+        df['emails'] = df.apply(lambda row: row['person_a_email'] + "; " + row['person_b_email'], axis=1)
+        df.drop(columns=['person_a', 'person_b', 'split_a', 'split_b'], inplace=True)
+        df.to_csv(ofile_name, index=False)
